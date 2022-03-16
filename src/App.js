6a837b51ab6bc9ccb0aaba6698c0023a4059cba2
foday <foday_s@yahoo.com>
2022-03-16 01:23:56 -0500

    useState constants, handleSearch function
    In the App.js file I imported useSearchBook custom hook and useState so I set a query and pageNum variable or state. The handleSearch function was created and in its param there is an e for event which is placed in the setQuery method or function for the useState the setPageNum function has a 1 in its params. The useSearchBook gets the query and pageNum from its param when text is entered into the input box..the event for the handleSearch is someone typing into the box then setQuery gets the data and stores it in query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React, {useState} from 'react'
+import useSearchBook from './useSearchBook';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNum, setPageNum] = useState(1);
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNum(1)
+  }
+
+  useSearchBook(query, pageNum)
   return (
     <div className="App">
       <h1>Search books</h1>
-      <input type="text"></input>
+      <input type="text" onChange={handleSearch}></input>
     </div>
   );
 }
--- src/useSearchBook.js
+++ src/useSearchBook.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 export default function useSearchBook(query, pageNum) {
@@ -6,7 +6,7 @@ export default function useSearchBook(query, pageNum) {
     useEffect(() => {
         axios({
             method: 'GET',
-            url: 'http//:openlibrary.org/search.json',
+            url: 'http://openlibrary.org/search.json',
             params: {q: query, page: pageNum }
         }).then(res => {
             console.log(res.data)
