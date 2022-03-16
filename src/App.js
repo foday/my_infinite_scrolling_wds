becf2b9331fc3187d5c3befc63694418fd23b421
foday <foday_s@yahoo.com>
2022-03-15 23:46:05 -0500

    beginning for programming
    npm create react app then i deleted files not needed and ran program

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Search books</h1>
+      <input type="text"></input>
+    </div>
+  );
+}
+
+export default App;
